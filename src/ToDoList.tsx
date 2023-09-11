d48fb7170ed6f2e3a4c2845a602299a612e5c1fa
sryung <sryung1225@gmail.com>
2023-09-11 22:15:48 +0900

    [📚🥁] React+TypeScript 로 Form 만들기

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 function ToDoList() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setToDo(event.currentTarget.value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
