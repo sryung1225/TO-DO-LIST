3d33241f0b515e8c82087d979696dad22b2ad7d0
sryung <sryung1225@gmail.com>
2023-09-12 23:35:39 +0900

    [🥁] 할 일 상태 변경하기 (1) 상태 버튼 조건부 렌더링 + 상태 인식

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,7 +1,32 @@
 import { IToDo } from "../atoms";
 
-function ToDo({ text }: IToDo) {
-  return <li>{text}</li>;
+function ToDo({ text, category }: IToDo) {
+  const changeCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    console.log(name);
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={changeCategory}>
+          DOING
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={changeCategory}>
+          TO DO
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={changeCategory}>
+          DONE
+        </button>
+      )}
+    </li>
+  );
 }
 
 export default ToDo;
