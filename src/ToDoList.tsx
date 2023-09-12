a805855ed89dc80f1f7ea4f6b43c5c1661209ed6
sryung <sryung1225@gmail.com>
2023-09-12 17:49:01 +0900

    [🥁] REACT-HOOK-FORMS 를 이용해서 투두리스트 폼 구성하기

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,83 +1,28 @@
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IFormData>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
-  const onValid = (data: IFormData) => {
-    if (data.password !== data.passwordConfirm) {
-      setError(
-        "passwordConfirm",
-        { message: "Password is different" },
-        { shouldFocus: true }
-      );
-    }
-    setError("extraError", { message: "Server is offline" });
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
   return (
     <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
+      <form onSubmit={handleSubmit(handleValid)}>
         <input
-          {...register("name", {
-            required: "write here",
-            validate: {
-              noNico: (value) =>
-                value.includes("nico") ? "no nicos allowed" : true,
-              noNick: (value) =>
-                value.includes("nick") ? "no nicks allowed" : true,
-            },
+          {...register("toDo", {
+            required: "Please write a to do",
           })}
-          placeholder="name"
+          placeholder="Write a to do"
         />
-        <span>{errors?.name?.message}</span>
-        <input
-          {...register("password", {
-            required: true,
-            minLength: 10,
-            maxLength: 20,
-          })}
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("passwordConfirm", {
-            required: true,
-          })}
-        />
-        <span>{errors?.passwordConfirm?.message}</span>
-        <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com email allowed",
-            },
-          })}
-          placeholder="123@naver.com"
-        />
-        <span>{errors?.email?.message}</span>
         <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
 }
 
-interface IFormData {
-  name: string;
-  email: string;
-  password: string;
-  passwordConfirm: string;
-  extraError?: string;
+interface IForm {
+  toDo: string;
 }
 
 export default ToDoList;
